a683bb32fd806d4b7b0014e415a36892fa7ded7f
mx.smblt <mx.smblt@gmail.com>
2023-11-12 17:07:18 +0200

    Changed the configuration for `gradle-license-report` plugin.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -21,6 +21,6 @@ dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.1")
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.2")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
-    implementation("com.github.jk1:gradle-license-report:2.2")
+    implementation("com.github.jk1:gradle-license-report:2.5")
     implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.13.2")
 }
